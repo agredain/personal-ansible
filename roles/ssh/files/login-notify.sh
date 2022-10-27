43d14d6788dadadccc0e23365e6fcd38e2c1a492
Daniel Agredano <agredain@gmail.com>
2022-10-27 14:27:57 +0200

    add ssh role

---
+++ roles/ssh/files/login-notify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# prepare any message you want
+login_ip="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
+login_date="$(date +"%e %b %Y, %a %r")"
+login_name="$(whoami)"
+
+# For new line I use $'\n' here
+message="New login to server"$'\n'"$login_name"$'\n'"$login_ip"$'\n'"$login_date"
+
+/media/data/scripts/telegram-notify --error --text "$message"
